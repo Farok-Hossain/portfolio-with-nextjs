f99842aec2a163b19cd1f0c97c3dab49420237ea
Farok Hossain <farokafs7@gmail.com>
2024-05-28 23:57:02 +0600

    updated hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,6 +18,11 @@ const Hero = () => {
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
+      <div className="flex justify-center">
+        <div className="max-w-[89vw]">
+          <h1>Dynamic Web Magic with Next.js</h1>
+        </div>
+      </div>
     </div>
   );
 };
