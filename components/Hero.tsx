8020a4c5dd18e17c4432d8abf5b4854645d5d590
Farok Hossain <farokafs7@gmail.com>
2024-05-29 00:11:31 +0600

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,8 +18,8 @@ const Hero = () => {
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
-      <div className="flex justify-center">
-        <div className="max-w-[89vw]">
+      <div className="flex justify-center relative my-20 z-10 ">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
