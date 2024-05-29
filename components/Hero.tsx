c52b4f372eb77c1dc580d392f7bd000f31379ffb
Farok Hossain <farokafs7@gmail.com>
2024-05-29 12:44:49 +0600

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -28,13 +29,17 @@ const Hero = () => {
 
           <TextGenerateEffect
             className=" text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Farok Hossain, a MERN Stack Developer
           </p>
           <a href="#about">
-            <MagicButton title="Show my work" />
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
           </a>
         </div>
       </div>
