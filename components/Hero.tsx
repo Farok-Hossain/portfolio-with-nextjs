9a84e00a6c315211ffe5feed255fc138ce63a55a
Farok Hossain <farokafs7@gmail.com>
2024-05-30 02:05:44 +0600

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-32">
+    <div className="pb-20 pt-36">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
