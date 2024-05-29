86b22665410fa2b2dc23d6e5e0b42afe7ddc23bf
Farok Hossain <farokafs7@gmail.com>
2024-05-29 10:30:51 +0600

    MagicButton added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
 
 const Hero = () => {
   return (
@@ -29,6 +30,12 @@ const Hero = () => {
             className=" text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Transforming Concepts into Seamless Experiences"
           />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Farok Hossain, a MERN Stack Developer
+          </p>
+          <a href="#about">
+            <MagicButton title="Show my work" />
+          </a>
         </div>
       </div>
     </div>
