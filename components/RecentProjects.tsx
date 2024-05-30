eb838356ddf3b31d45af3723528a6cf860a30447
Farok Hossain <farokafs7@gmail.com>
2024-05-30 23:42:29 +0600

    updated

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -17,12 +17,13 @@ const RecentProjects = () => {
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
             <PinContainer title={title} href={link}>
-              <div className=" relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]">
-                <div>
+              <div className=" relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                <div className=" relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
                 <img src={img} alt={title} className="z-10 absolute bottom-0" />
               </div>
+              <h1>{title}</h1>
             </PinContainer>
           </div>
         ))}
