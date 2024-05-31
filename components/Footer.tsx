3b21c790a3063835d73dac5045a9d0f0135bcdcf
Farok Hossain <farokafs7@gmail.com>
2024-06-01 01:09:52 +0600

    footer completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
 
 const Footer = () => {
   return (
@@ -28,6 +29,22 @@ const Footer = () => {
             position="right"
           />
         </a>
+      </div>
+      <div className=" flex mt-16 md:flex-row flex-col justify-between items-center ">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Farok
+        </p>
+
+        <div className=" flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <div
+              key={profile.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={profile.img} alt={profile.id} width={20} height={20} />
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );
