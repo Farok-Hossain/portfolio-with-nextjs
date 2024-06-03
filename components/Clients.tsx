7bffe28a7a982980364304ef741eccef8a631d72
Farok Hossain <farokafs7@gmail.com>
2024-06-03 21:46:43 +0600

    updated

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -7,13 +7,13 @@ import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const Clients = () => {
   return (
-    <section id="testimonials" className="py-24">
+    <section id="testimonials" className="py-20">
       <h1 className="heading">
         Kind words from
         <span className="text-purple"> satisfied clients</span>
       </h1>
 
-      <div className="flex flex-col items-center max-lg:mt-10">
+      <div className="flex flex-col items-center max-lg:mt-12">
         <div
           // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
           className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
