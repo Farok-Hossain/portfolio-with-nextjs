8b809660a2944bd07f7cbc041f9e824ea200ec1b
Farok Hossain <farokafs7@gmail.com>
2024-06-03 21:43:51 +0600

    updated

--- data/index.ts
+++ data/index.ts
@@ -100,7 +100,7 @@ export const projects = [
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "/p4.png",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
+    link: "https://bistro-restaurant-866e3.web.app/",
   },
 ];
 
