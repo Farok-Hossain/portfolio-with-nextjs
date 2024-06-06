2b9333d2663f7a2b3c2e23e947745c3aee392692
Farok Hossain <farokafs7@gmail.com>
2024-06-06 21:49:51 +0600

    updated text font

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,7 +42,7 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+          <p className="uppercase font-bold tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with{" "}
             <span className="text-pink-500">Next.js</span>
           </p>
