efb28b1d6b0e7b52687b353bf9458421ae86fb97
Farok Hossain <farokafs7@gmail.com>
2024-06-07 21:59:38 +0600

    index name updated

--- data/index.ts
+++ data/index.ts
@@ -126,7 +126,7 @@ export const testimonials = [
   {
     quote:
       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "McGerk",
+    name: "McCarthy",
     title: "Director of RapidScale Technologies",
   },
   {
