1fb0418ea736a53d42dab03046735237ba9320e6
Farok Hossain <farokafs7@gmail.com>
2024-07-10 21:43:05 +0600

    personalData updated

--- utils/data/personal-data.ts
+++ utils/data/personal-data.ts
@@ -1,4 +1,11 @@
 export const personalData = {
+  email: "farokafs7@gmail.com",
+  phone: "+8801618603009",
+  address: "Shukrabad, Dhanmondi-32, Dhaka, Bangladesh",
+  github: "https://github.com/Farok-Hossain",
+  facebook: "https://www.facebook.com/profile.php?id=100006429429427&sk=about",
+  linkedIn: "https://www.linkedin.com/in/farok-hossain-008444202/",
+  twitter: "https://x.com/farok_afs",
   resume:
     "https://drive.google.com/file/d/1xUPIKV74nAwwEds4E8TEB_pt-NoHv0pv/view",
 };
