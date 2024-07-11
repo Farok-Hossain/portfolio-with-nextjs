d6e6f528b019cedfd82f5ac277bb412644a2529e
Farok Hossain <farokafs7@gmail.com>
2024-07-11 23:27:40 +0600

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 import Footer from "@/components/Footer";
+import Code from "@/components/Card/Code";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         <Footer />
+        <Code />
       </body>
     </html>
   );
