8cb110ec5d2589fb0fc34b7aaf658ad5af9844bb
Farok Hossain <farokafs7@gmail.com>
2024-07-13 01:55:50 +0600

    updated

--- data/index.ts
+++ data/index.ts
@@ -2,6 +2,7 @@ export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Experience", link: "#experience" },
+  { name: "Skills", link: "#skills" },
   { name: "Contact", link: "#contact" },
 ];
 
