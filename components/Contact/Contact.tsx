01cf5b16b42c2551f7fb2c2f48c91d4fe2b24bd9
Farok Hossain <farokafs7@gmail.com>
2024-07-15 18:02:08 +0600

    react-hook-form added

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,11 +1,15 @@
 "use client";
-import React, { useState } from "react";
+
+import { useForm } from "react-hook-form";
 import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm();
 
   return (
     <div>
@@ -24,7 +28,7 @@ const Contact = () => {
         />
         <label>Message</label>
         <input
-          type="email"
+          type="message"
           className="bg-[#10172d] w-full h-20 border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
         />
       </form>
