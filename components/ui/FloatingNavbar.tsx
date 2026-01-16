c0111a9ba1756edfab8b6e7e0bd245a06c495369
Farok Hossain <farokafs7@gmail.com>
2026-01-16 19:32:14 +0600

    updated

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -56,14 +56,12 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center w-[95%] space-x-4",
+          "flex gap-10 max-w-full md:min-w-[70vw] lg:min-w-full fixed z-[5000] inset-x-0 mx-auto px-10 py-7 border-b border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center w-[95%] space-x-4",
           className
         )}
         style={{
           backdropFilter: "blur(16px) saturate(180%)",
           backgroundColor: "rgba(17, 25, 40, 0.75)",
-          borderRadius: "12px",
-          border: "1px solid rgba(255, 255, 255, 0.125)",
         }}
       >
         {navItems.map((navItem: any, idx: number) => (
@@ -71,10 +69,10 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative  items-center font-bold flex space-x-1  text-pink-600 dark:hover:text-neutral-300 hover:text-neutral-500 "
+              "relative items-center font-bold flex gap-10 text-pink-600 dark:hover:text-neutral-300 hover:text-neutral-500 "
             )}
           >
-            <span className="text-xs md:text-sm lg:text-sm  !cursor-pointer ">
+            <span className="text-xs md:text-sm lg:text-sm !cursor-pointer ">
               {navItem.name}
             </span>
           </Link>
