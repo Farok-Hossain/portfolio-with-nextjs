0a3ee5c3f9b5ef8e3495ead036a6b1ae7c31339a
Farok Hossain <farokafs7@gmail.com>
2026-01-24 23:18:40 +0600

    updated

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -15,7 +15,7 @@ const Clients = () => {
 
       <div className="flex flex-col items-center max-lg:mt-12">
         <div
-          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
+          // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive
           className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased  items-center justify-center relative overflow-hidden"
         >
           <InfiniteMovingCards
